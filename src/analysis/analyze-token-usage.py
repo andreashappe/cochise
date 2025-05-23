a3f7c56fa7129f57e5189020b74d34de8fbfec6d
Andreas Happe <andreashappe@snikt.net>
2025-05-23 12:25:08 +0200

    make analyse script more robust

--- src/analysis/analyze-token-usage.py
+++ src/analysis/analyze-token-usage.py
@@ -8,6 +8,56 @@ from pathlib import Path
 from rich.console import Console
 from rich.table import Table
 
+def analyzse_usage_metadata(results, event, j):
+    if event in results.keys():
+        assert(results[event]['model_name'] == j['costs']['model_name'])
+        results[event]['duration'] += j['duration']
+        results[event]['total'] += j['costs']['usage_metadata']['total_tokens']
+        results[event]['prompt'] += j['costs']['usage_metadata']['input_tokens']
+        results[event]['completions'] += j['costs']['usage_metadata']['output_tokens']
+        results[event]['reasoning'] += 0
+        results[event]['cached'] += 0
+
+    else:
+        results[event] = {
+            'duration': j['duration'],
+            'model_name': j['costs']['model_name'],
+            'event': event,
+            'total': j['costs']['usage_metadata']['total_tokens'],
+            'prompt': j['costs']['usage_metadata']['input_tokens'],
+            'completions': j['costs']['usage_metadata']['output_tokens'],
+            'reasoning': 0,
+            'cached': 0
+        }
+
+def analyzse_token_usage(results, event, j):
+    if j['costs']['token_usage']['completion_tokens_details'] != None and 'reasoning' in j['costs']['token_usage']['completion_tokens_details']:
+        reasoning_tokens = j['costs']['token_usage']['completion_tokens_details']['reasoning_tokens']
+    else:
+        reasoning_tokens = 0
+    
+    if event in results.keys():
+        assert(results[event]['model_name'] == j['costs']['model_name'])
+        results[event]['duration'] += j['duration']
+        results[event]['total'] += j['costs']['token_usage']['total_tokens']
+        results[event]['prompt'] += j['costs']['token_usage']['prompt_tokens']
+        results[event]['completions'] += j['costs']['token_usage']['completion_tokens']
+        results[event]['reasoning'] += reasoning_tokens
+        results[event]['cached'] += j['costs']['token_usage']['prompt_tokens_details']['cached_tokens']
+
+    else:
+        results[event] = {
+            'duration': j['duration'],
+            'model_name': j['costs']['model_name'],
+            'event': event,
+            'total': j['costs']['token_usage']['total_tokens'],
+            'prompt': j['costs']['token_usage']['prompt_tokens'],
+            'completions': j['costs']['token_usage']['completion_tokens'],
+            'reasoning': reasoning_tokens,
+            'cached': j['costs']['token_usage']['prompt_tokens_details']['cached_tokens'],
+        }
+
+
 def analyzse_file(filename):
 
     results = {}
@@ -18,26 +68,13 @@ def analyzse_file(filename):
 
             if 'costs' in j:
                 event = j['event']
-                if event in results.keys():
-                    assert(results[event]['model_name'] == j['costs']['model_name'])
-                    results[event]['duration'] += j['duration']
-                    results[event]['total'] += j['costs']['token_usage']['total_tokens']
-                    results[event]['prompt'] += j['costs']['token_usage']['prompt_tokens']
-                    results[event]['completions'] += j['costs']['token_usage']['completion_tokens']
-                    results[event]['reasoning'] += j['costs']['token_usage']['completion_tokens_details']['reasoning_tokens']
-                    results[event]['cached'] += j['costs']['token_usage']['prompt_tokens_details']['cached_tokens']
 
-                else:
-                    results[event] = {
-                        'duration': j['duration'],
-                        'model_name': j['costs']['model_name'],
-                        'event': event,
-                        'total': j['costs']['token_usage']['total_tokens'],
-                        'prompt': j['costs']['token_usage']['prompt_tokens'],
-                        'completions': j['costs']['token_usage']['completion_tokens'],
-                        'reasoning': j['costs']['token_usage']['completion_tokens_details']['reasoning_tokens'],
-                        'cached': j['costs']['token_usage']['prompt_tokens_details']['cached_tokens'],
-                    }
+                if 'token_usage' in j['costs']:
+                    analyzse_token_usage(results, event, j)
+                elif 'usage_metadata' in j['costs']:
+                    # if we don't have token usage, we can still get some information from here
+                    analyzse_usage_metadata(results, event, j)
+
 
     return results
 
