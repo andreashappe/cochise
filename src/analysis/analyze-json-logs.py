f2092baacf710514bc377e0810a74bab98b89529
Andreas Happe <andreashappe@snikt.net>
2025-05-31 15:27:25 +0200

    unify table output

--- src/analysis/analyze-json-logs.py
+++ src/analysis/analyze-json-logs.py
@@ -3,9 +3,16 @@
 import argparse
 
 from common import traverse_file, my_mean, my_std_dev
-
+from dataclasses import dataclass, field
 from rich.console import Console
 from rich.table import Table
+from typing import Dict, List, Set
+
+@dataclass
+class OutputTable:
+    title: str
+    headers: List[str]
+    rows: List[List[str]]
 
 def calc_costs(model, input_tokens, output_tokens, reasoning_tokens, cached_tokens):
     match model:
@@ -17,25 +24,10 @@ def calc_costs(model, input_tokens, output_tokens, reasoning_tokens, cached_toke
             print(f"Unknown model {model} for cost calculation")
             return 0.0
 
-def analysis_run_overview(console, input_files):
-    table = Table(title=f"Run Information")
-
-    table.add_column("Filename", justify="right", style="cyan", no_wrap=True)
-    table.add_column("Model", style="magenta")
-    table.add_column("Duration", justify="right", style="green")
-    table.add_column("Rounds", justify="right", style="green")
-    table.add_column("Mean Executor-Calls/Round", justify="right", style="green")
-    table.add_column("Dev Executor-Calls/Round", justify="right", style="green")
-    table.add_column("Mean Commands/Round", justify="right", style="green")
-    table.add_column("Dev Commands/Round", justify="right", style="green")
-    table.add_column("Planner Input", justify="right", style="green")
-    table.add_column("Planner Output", justify="right", style="green")
-    table.add_column("Exeuctor Input", justify="right", style="green")
-    table.add_column("Executor Output", justify="right", style="green")
-    table.add_column("Est. Cost", justify="right", style="green")
-
+def analysis_run_overview(console, input_files) -> List[OutputTable]:
     valid = 0
     invalid = 0
+    rows : List[List[str]] = []
 
     for i in args.input:
         result = traverse_file(i)
@@ -70,7 +62,7 @@ def analysis_run_overview(console, input_files):
                         executor_cached += data.cached_tokens
                         cost += calc_costs(data.model, data.prompt_tokens, data.completion_tokens, data.reasoning_tokens, data.cached_tokens)
 
-            table.add_row(
+            rows.append([
                 result.filename,
                 result.models_str(),
                 result.duration_str(),
@@ -84,30 +76,24 @@ def analysis_run_overview(console, input_files):
                 str(round(executor_input / 1000, 2)),  # Convert to kTokens
                 str(round(executor_output / 1000, 2)),  # Convert to kTokens
                 str(round(cost, 2))  # Cost in USD
-            )
+            ])
             valid += 1
         else:
             print(f"- {result.filename} has no valid models or strategy rounds")
             invalid += 1
 
-    console = Console() 
-    console.print(table)
-    console.print(f"Valid runs: {valid} Invalid runs: {invalid}") 
+    print(f"Valid runs: {valid} Invalid runs: {invalid}") 
+    return [OutputTable(
+        title="Run Information",
+        headers = ["Filename", "Model", "Duration", "Rounds", "Mean Executor-Calls/Round", "Dev Executor-Calls/Round", "Mean Commands/Round", "Dev Commands/Round", "Planner Input", "Planner Output", "Exeuctor Input", "Executor Output", "Est. Cost"],
+        rows = rows
+    )]
 
 def analysis_run_stats(console, input_files):
-    table = Table(title=f"Run Information")
-
-    table.add_column("Filename", justify="right", style="cyan", no_wrap=True)
-    table.add_column("Model", style="magenta")
-    table.add_column("Duration", justify="right", style="green")
-    table.add_column("Rounds", justify="right", style="green")
-    table.add_column("Mean Executor-Calls/Round", justify="right", style="green")
-    table.add_column("Dev Executor-Calls/Round", justify="right", style="green")
-    table.add_column("Mean Commands/Round", justify="right", style="green")
-    table.add_column("Dev Commands/Round", justify="right", style="green")
-
     valid = 0
     invalid = 0
+
+    rows : List[List[str]] = []
 
     for i in args.input:
         result = traverse_file(i)
@@ -117,7 +103,7 @@ def analysis_run_stats(console, input_files):
             executor_calls = [r.executor_llm_calls for r in result.rounds]
             tool_calls = [r.tool_calls for r in result.rounds]
 
-            table.add_row(
+            rows.append([
                 result.filename,
                 result.models_str(),
                 result.duration_str(),
@@ -126,32 +112,29 @@ def analysis_run_stats(console, input_files):
                 str(round(my_std_dev(executor_calls),2)),
                 str(round(my_mean(tool_calls), 2)),
                 str(round(my_std_dev(tool_calls), 2))
-            )
+            ])
             valid += 1
         else:
             print(f"- {result.filename} has no valid models or strategy rounds")
             invalid += 1
 
-    console = Console() 
-    console.print(table)
-    console.print(f"Valid runs: {valid} Invalid runs: {invalid}") 
+    print(f"Valid runs: {valid} Invalid runs: {invalid}") 
+    return [OutputTable(
+        title =f"Run Information",
+        headers = ["Filename", "Model", "Duration", "Rounds", "Mean Executor-Calls/Round", "Dev Executor-Calls/Round", "Mean Commands/Round", "Dev Commands/Round"],
+        rows = rows
+    )]
 
 def analysis_token_usage(console, input_files):
+
+    tables : List[OutputTable] = []
+
     for i in input_files:
         results = traverse_file(i)
-
-        table = Table(title=f"Run Information: {results.filename}")
-        table.add_column("Prompt", justify="right", style="cyan", no_wrap=True)
-        table.add_column("Model", style="magenta")
-        table.add_column("Total Tokens", justify="right", style="green")
-        table.add_column("Prompt Tokens", justify="right", style="green")
-        table.add_column("Completions Tokens", justify="right", style="green")
-        table.add_column("Reasoning Tokens", justify="right", style="green")
-        table.add_column("Cached Tokens", justify="right", style="green")
-        table.add_column("Duration", justify="right", style="green")
+        rows : List[List[str]] = []
 
         for event ,acc in results.tokens.items():
-            table.add_row(
+            rows.append([
                 event,
                 acc.model,
                 str(acc.total_tokens),
@@ -160,25 +143,44 @@ def analysis_token_usage(console, input_files):
                 str(acc.reasoning_tokens),
                 str(acc.cached_tokens),
                 str(round(acc.duration, 2))
-            )
-        console.print(table)
+            ])
+
+        tables.append(OutputTable(
+            title=f"Run Information: {results.filename}",
+            headers=["Prompt", "Model", "Total Tokens", "Prompt Tokens", "Completions Tokens", "Reasoning Tokens", "Cached Tokens", "Duration"],
+            rows=rows
+        ))
+
+    return tables
 
 if __name__=='__main__':
 
     console = Console()
 
     parser=argparse.ArgumentParser()
-    parser.add_argument('analysis', choices=['run-stats', 'token-usage', 'run-overview'], help='type of analysis to perform')
+    parser.add_argument('analysis', choices=['index-rounds', 'index-rounds-and-tokens', 'show-tokens'], help='type of analysis to perform')
     parser.add_argument('input', type=argparse.FileType('r'), nargs='+', help='input file to analyze')
     args = parser.parse_args()
 
+    console = Console() 
+    results = []
+
     match args.analysis:
-        case 'run-stats':
-            analysis_run_stats(console, args.input)
-        case 'token-usage':
-            analysis_token_usage(console, args.input)
-        case 'run-overview':
-            analysis_run_overview(console, args.input)
+        case 'index-rounds':
+            results = analysis_run_stats(console, args.input)
+        case 'show-tokens':
+            results = analysis_token_usage(console, args.input)
+        case 'index-rounds-and-tokens':
+            results = analysis_run_overview(console, args.input)
         case _:
             print(f"Unknown analysis type: {args.analysis}")
             exit(1)
+
+    for table in results:
+        t = Table(title=table.title)
+        for h in table.headers:
+            t.add_column(h)
+        for r in table.rows:
+            t.add_row(*r)
+        console.print(t)
+
